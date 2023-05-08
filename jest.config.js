920db9ddcef85e30463ac79d1395e526d7b8a7f4
Bo Li <libodev97@gmail.com>
2023-05-09 07:15:50 +0800

    06 Support using Webpack CSS Modules with Jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
-    '.(css|scss)$': require.resolve('./test/mock-css.js'),
+    '\\.module.(css|scss)$': 'identity-obj-proxy',
+    '\\.(css|scss)$': require.resolve('./test/mock-css.js'),
   },
 }
