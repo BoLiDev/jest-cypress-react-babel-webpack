17a3ad43992d3efe63bbfa89d5f24c3baf324a17
Bo Li <libodev97@gmail.com>
2023-05-10 06:27:10 +0800

    08 Support Custom Module Resolution with Jest moduleDirectories

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+const path = require('path')
+
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'shared'],
   moduleNameMapper: {
     '\\.module.(css|scss)$': 'identity-obj-proxy',
     '\\.(css|scss)$': require.resolve('./test/mock-css.js'),
